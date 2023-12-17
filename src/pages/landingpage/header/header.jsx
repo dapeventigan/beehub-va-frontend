c3ff1d2edd4dc612c60debeb42659b387c8c3d34
dapeventigan <dabeventigan@gmail.com>
2023-12-17 15:51:24 +0800

    log reset none httponly

--- src/pages/landingpage/header/header.jsx
+++ src/pages/landingpage/header/header.jsx
@@ -14,6 +14,7 @@ const Header = () => {
       withCredentials: true,
     }).then((res) => {
       console.log("Server response:", res.data);
+      
 
       // if (res.data !== "User not found") {
       //   setIsUserLoggedIn(true);
