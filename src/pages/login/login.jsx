8bc1189802bbba6a091ff247db9c73b83796e638
dapeventigan <dabeventigan@gmail.com>
2023-12-22 14:23:08 +0800

    try server beehub

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -22,7 +22,7 @@ const Login = () => {
     e.preventDefault();
 
     try {
-      await Axios.post("https://dape-beehub-va-api.onrender.com/login", {
+      await Axios.post("https://server.beehubvas.com/login", {
         email,
         password,
       }).then(async (res) => {
